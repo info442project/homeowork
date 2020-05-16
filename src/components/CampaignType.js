2307c4f5cc9491ededd96309a5473906134a4d8a
magalj <magalj@uw.edu>
2020-05-16 16:45:44 -0700

    bootstrap and campaign type

--- src/components/CampaignType.js
+++ src/components/CampaignType.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import Header from './header';
-import data from './yourdata';
+import React from "react";
+import Header from "./header";
+import data from "./yourdata";
+import "../App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { Button } from "react-bootstrap";
 
 const CampaignType = () => {
   return (
@@ -8,15 +11,18 @@ const CampaignType = () => {
       <Header name={data.name} contactEmail={data.contactEmail}></Header>
       <div className="about_container">
         <h1>1. What is your campaign type?</h1>
+        <p>* required information</p>
         <form>
           <label>
             <input type="text" name="name" />
           </label>
           <input type="submit" value="Submit" />
         </form>
-
+        <div ClassName="next_button">
+          <Button variant="primary">Next</Button>
+        </div>
       </div>
     </div>
   );
-}
+};
 export default CampaignType;
