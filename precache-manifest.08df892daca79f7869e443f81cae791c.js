794c78e6723400ad223009966f6930dfd746a3b9
skymrsn <46433941+skymrsn@users.noreply.github.com>
2020-05-24 16:15:40 -0700

    Update precache-manifest.08df892daca79f7869e443f81cae791c.js

--- precache-manifest.08df892daca79f7869e443f81cae791c.js
+++ precache-manifest.08df892daca79f7869e443f81cae791c.js
@@ -1,38 +1,38 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "38a53c646b27515938d108f4a91e07e1",
-    "url": "/index.html"
+    "url": "/homeowork/index.html"
   },
   {
     "revision": "58b79a7f2265f12c052f",
-    "url": "/static/css/main.d21f9780.chunk.css"
+    "url": "/homeowork/static/css/main.d21f9780.chunk.css"
   },
   {
     "revision": "e61ffa2a52947eeb4eea",
-    "url": "/static/js/2.bcbaff48.chunk.js"
+    "url": "/homeowork/static/js/2.bcbaff48.chunk.js"
   },
   {
     "revision": "cdf6e6b34674f199330e65ec0e02d3ee",
-    "url": "/static/js/2.bcbaff48.chunk.js.LICENSE.txt"
+    "url": "/homeowork/static/js/2.bcbaff48.chunk.js.LICENSE.txt"
   },
   {
     "revision": "58b79a7f2265f12c052f",
-    "url": "/static/js/main.589a730f.chunk.js"
+    "url": "/homeowork/static/js/main.589a730f.chunk.js"
   },
   {
     "revision": "0ef1460328a53b041825",
-    "url": "/static/js/runtime-main.c8b6af16.js"
+    "url": "/homeowork/static/js/runtime-main.c8b6af16.js"
   },
   {
     "revision": "aedbc5d5cdfd396dc6c68d6989db03d0",
-    "url": "/static/media/Team.aedbc5d5.svg"
+    "url": "/homeowork/static/media/Team.aedbc5d5.svg"
   },
   {
     "revision": "a2dee9f26b44bf7ec810f759b472ee46",
-    "url": "/static/media/browse.a2dee9f2.png"
+    "url": "/homeowork/static/media/browse.a2dee9f2.png"
   },
   {
     "revision": "920d8a3a217d43382a3be34a3d080d12",
-    "url": "/static/media/create.920d8a3a.png"
+    "url": "/homeowork/static/media/create.920d8a3a.png"
   }
 ]);
