f094a55aca336596ace3e9952242ecae86b72e5a
Yuchen <yuchew6@uw.edu>
2020-05-24 00:19:50 -0700

    Updates

---
+++ precache-manifest.6e70f936462426bf57e82faa7dbc50f6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ce02928ab2d504132eb41d508a9785",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d72e1e13bdbc4cafac78",
+    "url": "/static/css/main.d21f9780.chunk.css"
+  },
+  {
+    "revision": "6b7322a77dfe14fb1b4d",
+    "url": "/static/js/2.460e4157.chunk.js"
+  },
+  {
+    "revision": "cdf6e6b34674f199330e65ec0e02d3ee",
+    "url": "/static/js/2.460e4157.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d72e1e13bdbc4cafac78",
+    "url": "/static/js/main.529ce7b8.chunk.js"
+  },
+  {
+    "revision": "0ef1460328a53b041825",
+    "url": "/static/js/runtime-main.c8b6af16.js"
+  },
+  {
+    "revision": "aedbc5d5cdfd396dc6c68d6989db03d0",
+    "url": "/static/media/Team.aedbc5d5.svg"
+  },
+  {
+    "revision": "a2dee9f26b44bf7ec810f759b472ee46",
+    "url": "/static/media/browse.a2dee9f2.png"
+  },
+  {
+    "revision": "920d8a3a217d43382a3be34a3d080d12",
+    "url": "/static/media/create.920d8a3a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7645cd2876d7f1104b3e3f266a9b2e2b.js"
+  "/precache-manifest.6e70f936462426bf57e82faa7dbc50f6.js"
 );
 
 self.addEventListener('message', (event) => {
