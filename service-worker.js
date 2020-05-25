244e67760b10d45c8305fcfd86d8460439c31d06
Yuchen <yuchew6@uw.edu>
2020-05-24 23:03:20 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c08c9dc8a0ece629bf3683f46f91cd5f.js"
+  "/precache-manifest.5a8d7a86649b37aa7e9143dd724141b7.js"
 );
 
 self.addEventListener('message', (event) => {
